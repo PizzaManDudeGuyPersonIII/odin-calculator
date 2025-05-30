e8c04e7ba0f57aec401ec8136974fcf7215fbf98
Brian Pizzoferrato <98502079+PizzaManDudeGuyPersonIII@users.noreply.github.com>
2025-05-30 19:52:12 -0400

    Javascript functionality

--- javascript.js
+++ javascript.js
@@ -9,79 +9,393 @@ buttonsbox.append();
 buttonsboxleft.append();
 buttonsboxright.append();
 
+let displayContent = "";
+let number1 = "";
+let number2 = "";
+let completedNumber = "";
+let operator = "";
+
 const display = document.createElement("p");
 display.classList.add("display");
 display.textContent = "display";
 displaybox.append(display);
 
-let count = 10;
+// 'Clear' functionality
 let button = document.createElement("button");
 button.textContent = "Clear";
 button.classList.add("buttonClear", "buttons");
+button.addEventListener("click", () => {
+    number1 = "";
+    number2 = "";
+    operator = "";
+    displayContent = "";
+    completedNumber = "";
+    display.textContent = " ";
+    displaybox.append(display);
+})
 buttonsboxleft.append(button);
 
-for (i=1; i<count;i++) {
-    button = document.createElement("button");
-    button.textContent = `${i}`;
-    button.classList.add("button1-9", "buttons");
-    buttonsboxleft.append(button);
-}
+// '1' functionality
+button = document.createElement("button");
+button.textContent = "1";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "1";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (operator !== "")
+    {
+        number2 = number2 + "1";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
 
+// '2' functionality
+button = document.createElement("button");
+button.textContent = "2";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "2";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (operator !== "")
+    {
+        number2 = number2 + "2";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '3' functionality
+button = document.createElement("button");
+button.textContent = "3";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "3";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (operator !== "")
+    {
+        number2 = number2 + "3";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '4' functionality
+button = document.createElement("button");
+button.textContent = "4";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "4";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (operator !== "")
+    {
+        number2 = number2 + "4";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '5' functionality
+button = document.createElement("button");
+button.textContent = "5";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "5";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number1 !== "" && operator !== "")
+    {
+        number2 = number2 + "5";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '6' functionality
+button = document.createElement("button");
+button.textContent = "6";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "6";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number1 !== "" && operator !== "")
+    {
+        number2 = number2 + "6";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '7' functionality
+button = document.createElement("button");
+button.textContent = "7";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "7";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number1 !== "" && operator !== "")
+    {
+        number2 = number2 + "7";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '8' functionality
+button = document.createElement("button");
+button.textContent = "8";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "8";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number1 !== "" && operator !== "")
+    {
+        number2 = number2 + "8";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '9' functionality
+button = document.createElement("button");
+button.textContent = "9";
+button.classList.add("button1-9", "buttons");
+button.addEventListener("click", () => {
+    if (operator === "")
+    {
+        number1 = number1 + "9";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number1 !== "" && operator !== "")
+    {
+        number2 = number2 + "9";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
+buttonsboxleft.append(button);
+
+// '0' functionality
 button = document.createElement("button");
 button.textContent = "0";
 button.classList.add("button-0", "buttons");
+button.addEventListener("click", () => {
+    if (number1 === "")
+    {
+        number1 = number1 + "0";
+        display.textContent = number1;
+        displaybox.append(display);
+    }
+    else if (number2 === "" && operator !== "")
+    {
+        number2 = number2 + "0";
+        display.textContent = number2;
+        displaybox.append(display);
+    }
+})
 buttonsboxleft.append(button);
 
+// '.' functionality
 button = document.createElement("button");
 button.textContent = ".";
 button.classList.add("buttons", "button1-9");
 buttonsboxleft.append(button);
 
+// '+' functionality
 button = document.createElement("button");
 button.textContent = "+";
 button.classList.add("buttonOperators", "buttons");
+button.addEventListener("click", () => {
+    operator = "add";
+})
 buttonsboxright.append(button);
 
+// '-' functionality
 button = document.createElement("button");
 button.textContent = "-";
 button.classList.add("buttonOperators", "buttons");
+button.addEventListener("click", () => {
+    operator = "subtract";
+})
 buttonsboxright.append(button);
 
+// '*' functionality
 button = document.createElement("button");
 button.textContent = "*";
 button.classList.add("buttonOperators", "buttons");
+button.addEventListener("click", () => {
+    operator = "multiply";
+})
 buttonsboxright.append(button);
 
+// '/' functionality
 button = document.createElement("button");
-button.textContent = "%";
+button.textContent = "/";
 button.classList.add("buttonOperators", "buttons");
+button.addEventListener("click", () => {
+    operator = "divide";
+})
 buttonsboxright.append(button);
 
+// '=' functionality
 button = document.createElement("button");
 button.textContent = "=";
 button.classList.add("buttons", "buttonEquals");
+button.addEventListener("click", () => {
+    operate(number1, operator, number2);
+})
 buttonsboxright.append(button);
 
+// add backspace button, use slice(-1)
+
 function add(a,b) {
-    return a + b;
+    if (completedNumber !== "")
+    {
+        completedNumber = b + completedNumber;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+
+    else 
+    {
+        completedNumber = a + b;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+    
+    number1 = "";
+    number2 = "";
+    operator = "";
 }
 
 function subtract(a,b) {
-    return a - b;
+    if (completedNumber !== "")
+    {
+        completedNumber = b - completedNumber;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+
+    else 
+    {
+        completedNumber = a - b;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+    
+    number1 = "";
+    number2 = "";
+    operator = "";
 }
 
 function multiply(a,b) {
-    return a * b;
+    if (completedNumber !== "")
+    {
+        completedNumber = b * completedNumber;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+
+    else 
+    {
+        completedNumber = a * b;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+    
+    number1 = "";
+    number2 = "";
+    operator = "";
 }
 
 function divide(a,b) {
-    return a / b;
+    if (completedNumber !== "")
+    {
+        completedNumber = b / completedNumber;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+
+    else 
+    {
+        completedNumber = a / b;
+        display.textContent = completedNumber;
+        displaybox.append(display);
+    }
+    
+    number1 = "";
+    number2 = "";
+    operator = "";
 }
 
-const number1 = 0;
-const number2 = 0;
-const operator = 0;
-
 function operate(number1, operator, number2) {
-    add(number1, number2);
+
+    if (operator === "add")
+    {   
+        if (number1 !== "" && completedNumber === "")
+            add(parseInt(number1), parseInt(number2));
+        else if (number1 === "" && completedNumber !== "")
+            add(completedNumber, parseInt(number2));
+    }
+    else if (operator === "subtract")
+    {
+        if (number1 !== "" && completedNumber === "")
+            subtract(parseInt(number1), parseInt(number2));
+        else if (number1 === "" && completedNumber !== "")
+            subtract(completedNumber, parseInt(number2));
+    }
+    else if (operator === "multiply")
+    {
+        if (number1 !== "" && completedNumber === "")
+            multiply(parseInt(number1), parseInt(number2));
+        else if (number1 === "" && completedNumber !== "")
+            multiply(completedNumber, parseInt(number2));
+    }
+    else if (operator === "divide")
+    {
+        if (number1 !== "" && completedNumber === "")
+            divide(parseInt(number1), parseInt(number2));
+        else if (number1 === "" && completedNumber !== "")
+            divide(completedNumber, parseInt(number2));
+    }
 }
